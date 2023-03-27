e160051104028474daa0df62cf21f0ade19f8bb3
Pointshock <christomaskovic9@gmail.com>
2023-03-27 12:43:59 -0700

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,102 @@
+# Description: This file contains the code for the camera module. It uses the YOLOv5 model to detect fire in real time.
+# Chris Tomaskovic and Anthony Shank
+# Import classes
+import torch
+import numpy as np
+import cv2
+from time import time
+
+
+class FireDetection:
+    """
+    Class implements Yolo5 model to make inferences on a youtube video using Opencv2.
+    """
+
+    def __init__(self, capture_index, model_name):
+        """
+        Initializes the class with youtube url and output file.
+        :param url: Has to be as youtube URL,on which prediction is made.
+        :param out_file: A valid output file name.
+        """
+        self.capture_index = capture_index
+        self.model = self.load_model(model_name)
+        self.classes = self.model.names
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("Using Device: ", self.device)
+
+    # Function to get the video capture object
+    def get_video_capture(self):
+        return cv2.VideoCapture(self.capture_index)
+
+    def load_model(self, model_name):
+        if model_name:
+            model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_name, force_reload=True)
+        else:
+            model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        return model
+
+    # Function to score the frame
+    def score_frame(self, frame):
+    
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+        return labels, cord
+
+    def class_to_label(self, x):
+        return self.classes[int(x)]
+
+    # Function to plot the boxes on the frame
+    def plot_boxes(self, results, frame):
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.3:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                bgr = (0, 0, 255)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+
+        return frame
+
+    def __call__(self):
+        """
+        This function is called when class is executed, it runs the loop to read the video frame by frame,
+        and write the output into a new file.
+        :return: void
+        """
+        cap = self.get_video_capture()
+        assert cap.isOpened()
+      
+        while True:
+          
+            ret, frame = cap.read()
+            assert ret
+            
+            # Resize the frame to 640x640
+            frame = cv2.resize(frame, (640,640))
+            
+            start_time = time()
+            results = self.score_frame(frame)
+            frame = self.plot_boxes(results, frame)
+            
+            end_time = time()
+            fps = 1/np.round(end_time - start_time, 2)
+            #print(f"Frames Per Second : {fps}")
+             
+            cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (150,150,0), 2)
+            
+            cv2.imshow('YOLOv5 Detection', frame)
+ 
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+      
+        cap.release()
+        
+        
+# Create a new object and execute.
+detector = FireDetection(capture_index=0, model_name='best.pt')
+detector()
